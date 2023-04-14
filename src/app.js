6e9b15a304021891054de71e04d602e30d6e6f0c
Mauricio Castro <mauriciodesouzaecastro@gmail.com>
2023-04-14 18:59:13 -0300

    participantes retirados da sala apos 10 segundos de inatividade

--- src/app.js
+++ src/app.js
@@ -119,7 +119,7 @@ app.get("/messages", async (req, res) => {
 
 app.post("/status", async (req, res) => {
     const User = req.headers.user || req.headers.User;
-    console.log(req.headers)
+    console.log(Date.now())
     if (!User) return res.sendStatus(404)
     try {
         const participante = await db.collection("participants").findOne({ name: User })
@@ -135,6 +135,26 @@ app.post("/status", async (req, res) => {
         res.status(200).send("Usuario atualizado");
     } catch (err) { return res.status(500).send(err.message); }
 })
+
+setInterval(async () => {
+    const tenSecondsAgo = Date.now() - 10000;
+    const participants = await db.collection("participants")
+        .find({ lastStatus: { $lt: tenSecondsAgo } }).toArray();
+    if (participants.length > 0) {
+        const participantNames = participants.map(p => p.name);
+        await db.collection("participants").deleteMany({ name: { $in: participantNames } });
+        for (const name of participantNames) {
+            const newMessage = {
+                from: name,
+                to: 'Todos',
+                text: 'sai da sala...',
+                type: 'status',
+                time: dayjs().format('HH:mm:ss')
+            };
+            await db.collection("messages").insertOne(newMessage);
+        }
+    }
+}, 15000);
 
 const port = 5000
 app.listen(5000, () => console.log(`servidor rodando na porta ${port}`))
